ae3d105c13e6b1725ac062f87b49971f105a686f
Prashanth <raopacs@icloud.com>
2025-07-20 16:38:23 +1000

    feat: first cut app

---
+++ PropertyTenancyTests/PropertyTenancyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyTenancyTests.swift
+//  PropertyTenancyTests
+//
+//  Created by Prashanth Rao on 20/7/2025.
+//
+
+import Testing
+@testable import PropertyTenancy
+
+struct PropertyTenancyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
